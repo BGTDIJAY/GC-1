c433540111e0438a2c86015bf89c3d0d7cf84096
BGTDIJAY <132917304+BGTDIJAY@users.noreply.github.com>
2026-05-06 14:13:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { Toaster } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip.tsx";
+const TooltipProvider = ({ children }: { children: React.ReactNode }) => <>{children}</>;
 import { CartProvider } from "./context/CartContext";
 import SplashScreen from "./components/SplashScreen";
 import SocialSidebar from "./components/SocialSidebar";
